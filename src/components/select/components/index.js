39add0560213d12d5e9170bde4834d45dbc0f9b4
Thomas Peace <thomas.peace@bigfinite.com>
2019-09-03 09:10:18 +0200

    add select folder

---
+++ src/components/select/components/index.js
@@ -0,0 +1,4 @@
+// overrides any components in this list: https://react-select.com/components
+
+export { default as NoOptionsMessage } from './no-options-message.jsx'
+export { default as IndicatorsContainer } from './indicator-container.jsx'
